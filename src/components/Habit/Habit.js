fab5c0dbd9832077aa0c463c3ea053e04d753563
Aiza ali <aiza9382@gmail.com>
2024-01-13 13:48:09 +0500

    updated habit.js file

--- src/components/Habit/Habit.js
+++ src/components/Habit/Habit.js
@@ -1,38 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import HabitCard from '../HabitCard/HabitCard';
 import Modal from "../Modal/Modal";
 import "./Habit.css";
-import axios from "axios";
 
 function Habit() {
   const [habits, setHabits] = useState([]);
-
-  useEffect(() => {
-    // Fetch habits data from API or perform any initial data loading logic
-    // Example:
-    // axios.get("/api/habits")
-    //   .then(response => {
-    //     setHabits(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
-
-    // For this example, I'll set sample data directly
-    // const sampleData = [
-    //   {
-    //     _id: "1",
-    //     habit: "Sample Habit 1",
-    //     completed: false,
-    //   },
-    //   {
-    //     _id: "2",
-    //     habit: "Sample Habit 2",
-    //     completed: true,
-    //   },
-    // ];
-    // setHabits(sampleData);
-  }, []);
 
   return (
     <div className="Habit">
